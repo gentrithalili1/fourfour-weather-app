aebf9a98751463edf121d1af98522f0c23f9f48e
gentrithalili1 <gentrithalili11@gmail.com>
2026-03-19 00:45:06 +0100

    refactor theme store types

--- src/core/stores/theme-store.ts
+++ src/core/stores/theme-store.ts
@@ -3,12 +3,15 @@ import { persist } from "zustand/middleware";
 import { generateStorageKey } from "@/core/utils/storage";
 import type { Theme } from "@/core/types/common";
 
-type ThemeStore = {
+type ThemeStoreState = {
   theme: Theme;
+};
+
+type ThemeStoreActions = {
   setTheme: (theme: Theme) => void;
 };
 
-export const useThemeStore = create<ThemeStore>()(
+export const useThemeStore = create<ThemeStoreState & ThemeStoreActions>()(
   persist(
     (set) => ({
       theme: "system",
