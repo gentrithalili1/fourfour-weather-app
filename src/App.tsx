4023edb3384fc84a8e22fe595cacd32595462763
gentrithalili1 <gentrithalili11@gmail.com>
2026-03-23 15:39:42 +0100

    add sonner alerts

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Weather } from "@/features/weather/views/weather";
 
 import { ReactQueryProvider } from "@/core/components/providers/react-query-provider";
 import { ThemeProvider } from "@/core/components/providers/theme-provider";
+import { Toaster } from "@/core/components/ui/sonner";
 
 function App() {
 	return (
 		<ReactQueryProvider>
 			<ThemeProvider>
 				<Weather />
+				<Toaster position="top-center" />
 			</ThemeProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</ReactQueryProvider>
